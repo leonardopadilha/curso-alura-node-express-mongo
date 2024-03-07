e6dc1fc7bb8f87350a677860890103a9baa19ab6
leonardopadilha <leonardopadilha28@gmail.com>
2024-03-07 06:26:51 -0300

    Atualizar Livro

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -3,8 +3,22 @@ import livro from "../models/livro.js";
 class LivroController {
 
     static async listarLivros(req, res) {
-        const listaLivros = await livro.find({});
-        res.status(200).json(listaLivros)
+        try {
+            const listaLivros = await livro.find({});
+            res.status(200).json(listaLivros)
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na requisição`})
+        }
+    }
+
+    static async listaLivroPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const livroEncontrado = await  livro.findById(id);
+            res.status(200).json(livroEncontrado);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na requisição do livro`})
+        }
     }
 
     static async cadastrarLivro(req, res) {
@@ -17,6 +31,16 @@ class LivroController {
                     });
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro`})
+        }
+    }
+
+    static async atualizarLivro(req, res) {
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "Livro atualizado"});
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na atualização`})
         }
     }
 };
