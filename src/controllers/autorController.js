c27fc920d8aa7d02493ef3c7c8504ae66acb82d8
leonardopadilha <leonardopadilha28@gmail.com>
2024-03-12 09:53:24 -0300

    Lista Author por Id

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -10,6 +10,16 @@ class AutorController {
             res.status(500).json({ message: `${erro.message} - falha na requisição`})
         }
     }
+
+    static async listaAutorPorId(req, res) {
+        try {
+            const id = req.params.id;
+            const autorEncontrado = await  autor.findById(id);
+            res.status(200).json(autorEncontrado);
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na requisição do autor`})
+        }
+    }
 };
 
 export default AutorController;
