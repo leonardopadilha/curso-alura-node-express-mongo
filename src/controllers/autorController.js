ec15481557943ecc75f16124ded62af3ec7fbfd4
leonardopadilha <leonardopadilha28@gmail.com>
2024-03-14 13:13:24 -0300

    Atualizar Autor

--- src/controllers/autorController.js
+++ src/controllers/autorController.js
@@ -33,6 +33,16 @@ class AutorController {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar autor`})
         }
     }
+
+    static async atualizarAutor(req, res) {
+        try {
+            const id = req.params.id;
+            await autor.findByIdAndUpdate(id, req.body);
+            res.status(200).json({ message: "autor atualizado"});
+        } catch (erro) {
+            res.status(500).json({ message: `${erro.message} - falha na atualização`})
+        }
+    }
 };
 
 export default AutorController;
