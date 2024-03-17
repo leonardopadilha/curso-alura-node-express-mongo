bf54253f71fc86aadc74550173eecc7e674c2d62
leonardopadilha <leonardopadilha28@gmail.com>
2024-03-17 08:34:15 -0300

    Autor rota - Listar autores por id

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -4,5 +4,6 @@ import AutorController from "../controllers/autorController.js";
 const routes = express.Router();
 
 routes.get("/autores", AutorController.listarAutores);
+routes.get("/autores/:id", AutorController.listaAutorPorId);
 
 export default routes;
