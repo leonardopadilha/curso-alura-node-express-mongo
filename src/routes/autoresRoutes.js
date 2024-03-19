2b2c18dbf44c3a9ddbb92af15b8e47af1b80d0d3
leonardopadilha <leonardopadilha28@gmail.com>
2024-03-19 08:05:07 -0300

    Autor rota - Atualizar e Excluir Autor

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -6,5 +6,7 @@ const routes = express.Router();
 routes.get("/autores", AutorController.listarAutores);
 routes.get("/autores/:id", AutorController.listaAutorPorId);
 routes.post("/autores", AutorController.cadastrarAutor);
+routes.put("/autores/:id", AutorController.atualizarAutor);
+routes.delete("/autores/:id", AutorController.excluirAutor)
 
 export default routes;
