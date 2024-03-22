e2cef4725a27d6e2163b3e06061e0663df8fb4dc
leonardopadilha <leonardopadilha28@gmail.com>
2024-03-22 08:36:42 -0300

    Ajuste livro Schema

--- src/models/livro.js
+++ src/models/livro.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import { autor, autorSchema } from "./autor.js";
 
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: String, required: true },
+    titulo: { type: String },
     editora: { type: String },
     preco: { type: Number },
-    paginas: { type: Number }
+    paginas: { type: Number },
+    autor: { type: mongoose.Schema.Types.ObjectId, ref: 'autores', required: true},
 }, { versionKey: false });
 
 const livro = mongoose.model("livros", livroSchema);
